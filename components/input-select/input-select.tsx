eb5550c518bcd258dced7e586f3ec3817873ffba
Leigh Kendell <leighkendell@gmail.com>
2019-03-07 23:05:57 +0800

    Add example of initial mutation/form handling for adding products

--- components/input-select/input-select.tsx
+++ components/input-select/input-select.tsx
@@ -3,10 +3,20 @@ import styles from './input-select.scss';
 
 interface Props extends React.SelectHTMLAttributes<HTMLSelectElement> {
   children: ReactNode;
+  label?: string;
 }
 
-const InputSelect: React.FunctionComponent<Props> = ({ children }) => (
-  <select className={styles.select}>{children}</select>
+const InputSelect: React.FunctionComponent<Props> = ({ children, onChange, value, required, id, label }) => (
+  <>
+    {label && (
+      <label htmlFor={id} className={styles.label}>
+        {label}
+      </label>
+    )}
+    <select id={id} className={styles.select} onChange={onChange} value={value} required={required}>
+      {children}
+    </select>
+  </>
 );
 
 export default InputSelect;
--- components/product-form/product-form.tsx
+++ components/product-form/product-form.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import { Button, InputSelect } from '..';
-import { ProductVariantConnection } from '../../interfaces';
+import React, { ChangeEvent, useState } from 'react';
+import { Mutation, MutationFn } from 'react-apollo';
+import { Button, CheckoutQuery, InputSelect } from '..';
+import { checkoutLineItemsReplace } from '../../graphql/checkout';
+import { Checkout, ProductVariantConnection } from '../../interfaces';
 
 interface Props {
   variants: ProductVariantConnection;
 }
 
-const ProductForm: React.FunctionComponent<Props> = ({ variants }) => (
-  <form onSubmit={() => console.log('woop')}>
-    <InputSelect>
-      {/* tslint:disable:react-a11y-role-has-required-aria-props */}
-      {variants.edges.map(variant => (
-        <option key={variant.node.id} value={variant.node.id}>
-          {variant.node.title}
-        </option>
-      ))}
-      {/* tslint:enable */}
-    </InputSelect>
-    <Button>Add to cart</Button>
-  </form>
-);
+interface FormSubmitParams {
+  event: Event;
+  mutate: MutationFn;
+  checkout: Checkout;
+}
+
+const ProductForm: React.FunctionComponent<Props> = ({ variants }) => {
+  const [productVariant, updateProductVariant] = useState('default');
+  const [productQuantity, updateProductQuantity] = useState('');
+
+  const handleVariantChange = (event: ChangeEvent) => {
+    const target = event.target as HTMLSelectElement;
+    updateProductVariant(target.value);
+  };
+
+  const handleFormSubmit = ({ event, mutate, checkout }: FormSubmitParams) => {
+    event.preventDefault();
+    mutate({
+      variables: {
+        checkoutId: checkout.id,
+        lineItems: [
+          {
+            variantId: productVariant,
+            quantity: 1,
+          },
+        ],
+      },
+    });
+
+    console.log(productVariant);
+  };
+
+  return (
+    <CheckoutQuery>
+      {checkout => {
+        return (
+          <Mutation mutation={checkoutLineItemsReplace}>
+            {mutate => (
+              <form onSubmit={() => handleFormSubmit({ event, mutate, checkout })}>
+                <InputSelect id="variant" value={productVariant} onChange={handleVariantChange} label="Size">
+                  {/* tslint:disable:react-a11y-role-has-required-aria-props */}
+                  <option disabled={true} value="default" />
+                  {variants.edges.map(variant => (
+                    <option key={variant.node.id} value={variant.node.id}>
+                      {variant.node.title}
+                    </option>
+                  ))}
+                  {/* tslint:enable */}
+                </InputSelect>
+                <Button>Add to cart</Button>
+              </form>
+            )}
+          </Mutation>
+        );
+      }}
+    </CheckoutQuery>
+  );
+};
 
 export default ProductForm;
