1bed50d57440730fd6a763b185e1344cd26882c3
Leigh Kendell <leighkendell@gmail.com>
2019-03-21 22:29:25 +0800

    Add updated icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,7 +14,14 @@ export default class CustomDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
           <link rel="stylesheet" href="https://use.typekit.net/nfn4mpw.css" />
           <meta name="theme-color" content="#2851dc" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/icons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
+          <link rel="mask-icon" href="/static/icons/safari-pinned-tab.svg" color="#000000" />
+          <link rel="shortcut icon" href="/static/icons/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#2851dc" />
           <link rel="manifest" href="/static/manifest.json" />
+          <meta name="msapplication-config" content="/static/browserconfig.xml" />
         </Head>
         <body>
           <Main />
