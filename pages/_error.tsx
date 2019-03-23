52c3c14424cf63deac8f41b89e2ff38d442a564b
Leigh Kendell <leighkendell@gmail.com>
2019-03-23 13:39:19 +0800

    Add custom error page

---
+++ pages/_error.tsx
@@ -0,0 +1,37 @@
+import { NextContext } from 'next';
+import React from 'react';
+import { HeaderText, Layout, Section, Text, Wrapper } from '../components';
+
+interface Props {
+  statusCode: number;
+}
+
+class Error extends React.Component<Props> {
+  public static getInitialProps({ res }: NextContext) {
+    const statusCode = res ? res.statusCode : null;
+    return { statusCode };
+  }
+
+  public render() {
+    const { statusCode } = this.props;
+    const title = statusCode ? (statusCode === 404 ? 'Not found' : 'Error') : 'Error';
+    const message = statusCode
+      ? statusCode === 404
+        ? `Oops, you just hit a page that doesn't exist.`
+        : `${statusCode}, something went wrong.`
+      : 'Oops, something went wrong';
+
+    return (
+      <Layout pageTitle={title}>
+        <HeaderText>{title}</HeaderText>
+        <Section>
+          <Wrapper collapseTop={true}>
+            <Text>{message}</Text>
+          </Wrapper>
+        </Section>
+      </Layout>
+    );
+  }
+}
+
+export default Error;
